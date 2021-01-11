2e82446caac6fb651e555dd91875b81116f73bb1
allenzpierre <pieall25@ecolecatholique.ca>
2021-01-11 19:17:05 +0000

    movement

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import pgzrun
 WIDTH = 800
 HEIGHT = 800
 
+itachi = Actor("itachi.png")
+itachi.pos - (0, HEIGHT / 2)
+speed - 1
+
 def draw():
   screen.fill("white")
   screen.blit("itachi", (300,145))
-
-
-
