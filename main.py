44c0def47f0c9aac0fcd0a2425d123a134b82a92
allenzpierre <pieall25@ecolecatholique.ca>
2021-01-13 15:00:49 +0000

    keys

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ WIDTH = 800
 HEIGHT = 800
 
 itachi = Actor("itachi.png")
-itachi.pos - (0, HEIGHT / 2)
-speed - 1
-
+keys = pygame.key.get_press()
+if keys[pygame.K_w]:
+    y-=1
+if keys [pygame.K_s]:
+    y+=1
+if keys[pygame.K_d]:
+    x+=1
+if keys[pygame.K_a]:
+    x-=1
+screen.blit(itachi, (x,y))
+pygame.display.flip()
 def draw():
   screen.fill("white")
   screen.blit("itachi", (300,145))
+  
+  pgzrun.go()
